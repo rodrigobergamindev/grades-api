4250a534613bf7ed13414b701aa269da0c36478e
Rodrigo <61331491+rodrigobergamindev@users.noreply.github.com>
2020-07-05 23:59:55 -0300

    dblog provide

--- config/logger.js
+++ config/logger.js
@@ -15,7 +15,7 @@ const logger = createLogger({
     new transports.Console(),
     new transports.MongoDB({
       level: 'info',
-      db: db.url,
+      db: process.env.MONGO_URL,
       collection: 'logs_grades',
       capped: true,
       cappedMax: 20,
